a62b7f725e735470aecbad606603bfa2cd99a282
fatime-eyvazova12 <fatima.eyvazovaa@gmail.com>
2024-09-23 11:40:02 +0400

    delete console.log

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import useFetch from "./hooks/useFetch";
 import Detail from "./pages/Detail/Detail";
 import Favorites from "./pages/Favorites/Favorites";
 
 const App: React.FC = () => {
-  const { data, loading, error } = useFetch(
-    "www.themealdb.com/api/json/v1/1/filter.php?a=Canadian"
-  );
   return (
     <Router>
       <Routes>
